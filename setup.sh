1a8676f8c9a88bafcc2b1b5d3c1ce41c9dde0779
spacesamurai422 <spacesamurai422@gmail.com>
2024-07-04 01:12:49 +0900

    Adding kubeconfig and encryption config generation

--- setup.sh
+++ setup.sh
@@ -51,4 +51,132 @@ for host in node-0 node-1; do
 
 done
 
-scp ca.key ca.crt kube-api-server.key kube-api-server.crt service-accounts.key service-accounts.crt admin@server:~/
+scp ca.key ca.crt kube-api-server.key kube-api-server.crt service-accounts.key service-accounts.crt admin@server:/tmp
+
+#Generate kubeconfig files
+
+#kubeconfig for kubelets
+for host in node-0 node-1; do
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.crt \
+    --embed-certs=true \
+    --server=https://server.kubernetes.local:6443 \
+    --kubeconfig=${host}.kubeconfig
+
+  kubectl config set-credentials system:node:${host} \
+    --client-certificate=${host}.crt \
+    --client-key=${host}.key \
+    --embed-certs=true \
+    --kubeconfig=${host}.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:node:${host} \
+    --kubeconfig=${host}.kubeconfig
+
+  kubectl config use-context default \
+    --kubeconfig=${host}.kubeconfig
+done
+
+#kubeconfig for kube-proxy service
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.crt \
+  --embed-certs=true \
+  --server=https://server.kubernetes.local:6443 \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-credentials system:kube-proxy \
+  --client-certificate=kube-proxy.crt \
+  --client-key=kube-proxy.key \
+  --embed-certs=true \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes-the-hard-way \
+  --user=system:kube-proxy \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config use-context default \
+  --kubeconfig=kube-proxy.kubeconfig
+
+#kubeconfig for kube-controller-manager service
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.crt \
+  --embed-certs=true \
+  --server=https://server.kubernetes.local:6443 \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-credentials system:kube-controller-manager \
+  --client-certificate=kube-controller-manager.crt \
+  --client-key=kube-controller-manager.key \
+  --embed-certs=true \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes-the-hard-way \
+  --user=system:kube-controller-manager \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config use-context default \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+#kubeconfig for kube-scheduler service
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.crt \
+  --embed-certs=true \
+  --server=https://server.kubernetes.local:6443 \
+  --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-credentials system:kube-scheduler \
+  --client-certificate=kube-scheduler.crt \
+  --client-key=kube-scheduler.key \
+  --embed-certs=true \
+  --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes-the-hard-way \
+  --user=system:kube-scheduler \
+  --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config use-context default \
+  --kubeconfig=kube-scheduler.kubeconfig
+
+#kubeconfig for admin user
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.crt \
+  --embed-certs=true \
+  --server=https://127.0.0.1:6443 \
+  --kubeconfig=admin.kubeconfig
+
+kubectl config set-credentials admin \
+  --client-certificate=admin.crt \
+  --client-key=admin.key \
+  --embed-certs=true \
+  --kubeconfig=admin.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes-the-hard-way \
+  --user=admin \
+  --kubeconfig=admin.kubeconfig
+
+kubectl config use-context default \
+  --kubeconfig=admin.kubeconfig
+
+#Copy the kubelet and kube-proxy configs to their respective nodes
+for host in node-0 node-1; do
+  ssh admin@$host "sudo mkdir /var/lib/{kube-proxy,kubelet}"
+
+  scp -o StrictHostKeyChecking=no kube-proxy.kubeconfig admin@$host:/tmp/kubeconfig
+  ssh -o StrictHostKeyChecking=no admin@$host sudo mv /tmp/kubeconfig /var/lib/kube-proxy/
+
+  scp -o StrictHostKeyChecking=no ${host}.kubeconfig admin@$host:/tmp/kubeconfig
+  ssh -o StrictHostKeyChecking=no admin@$host sudo mv /tmp/kubeconfig /var/lib/kubelet/
+
+done
+
+scp admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig admin@server:/tmp
+
+#Generate encryption config
+export ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+envsubst < configs/encryption-config.yaml > encryption-config.yaml
+scp encryption-config.yaml admin@server:/tmp
